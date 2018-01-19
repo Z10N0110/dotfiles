1b2fd786992a4a298aa1da49fd5de12e0d223df6
Z10N0110 <dungeoncog@outlook.com>
2018-01-19 16:58:53 +0800

    feat: more complex random password generator

--- .scripts/rdpswd.lua
+++ .scripts/rdpswd.lua
@@ -1,10 +1,12 @@
 #!/usr/bin/env lua
+-- [a-z], [A-Z], [0-9], [()`~!@#$%^&*-+=_|{}[]:;'<>,.?/]
+local pw_constraint = {
+    ['abcdefghijklmnopqrstuvwxyz'] = 2,
+    ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'] = 2,
+    ['0123456789'] = 2,
+    ['_+-&=!@#$%^*()'] = 2,
+}
 
-local ascii = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-local digits = '0123456789'
-local special_chars = '_+-&=!@#$%^*()'
-
-local fileds = ascii .. digits .. special_chars
 local pw_len = 16
 local pw_table = {}
 
@@ -18,9 +20,32 @@ end
 local seed = tostring(os.time()):reverse():sub(1, 6) .. v
 math.randomseed(seed)
 
-for i = 1, pw_len do 
+function is_exist(value, tab)
+  for k,v in ipairs(tab) do
+    if v == value then
+        return true
+    end
+  end
+  return false
+end
+
+function uniq_update(fileds, count)
+  while true do
     local random_no = math.random(#fileds)
     local pw_str = string.sub(fileds,random_no,random_no)
-    pw_table[i] = pw_str
+    if not is_exist(pw_str, pw_table) then
+      pw_table[count] = pw_str
+      return count + 1
+    end
+  end
+end
+
+local count = 1
+while count <= pw_len do
+  for fileds, must in pairs(pw_constraint) do
+    for j = 1, must do 
+      count = uniq_update(fileds, count)
+    end
+  end
 end
 print(table.concat(pw_table))
